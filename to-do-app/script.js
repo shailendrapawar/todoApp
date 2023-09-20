bd2821f22ead1dc242999a5fe9eed47118f515bd
Kartik Ginwal <114296676+untuned07@users.noreply.github.com>
2023-09-20 17:44:29 +0530

    Updated Script

--- to-do-app/script.js
+++ to-do-app/script.js
@@ -3,57 +3,39 @@ let button = document.getElementById("button")
 let listBody = document.querySelector(".list-body")
 let count = document.querySelector(".count");
 let countValue = 0;
-
-
-
+countValue = Number.parseInt(countValue);
 
 button.addEventListener("click", () => {
-
     if (input.value == "") {
         alert("enter some value first")
-    }
-    else {
-
+    } else {
         let inputValue = input.value;
         let li = document.createElement("li")
         li.classList.add('item')
         li.innerHTML = `
-
             ${inputValue}
             <span id="icon-body" class="icon-body"><i id="icon-btn" class="fa-solid fa-trash fa-2xl"></i></span>
         `
-
         listBody.appendChild(li)
-        countValue=countValue+1;
+        countValue++;
         count.innerHTML = `todo count : ${countValue}`
         input.value = ""
         saveData();
-
     }
 })
 
 listBody.addEventListener(("click"),(e)=>{
-    // console.log(e.target.tagName)
     if(e.target.tagName=="LI"){
-        // console.log(e.target.classList)
-        // console.log("it being pressed")
         e.target.classList.toggle("checked");
         saveData()
-
     }
     else if(e.target.tagName=="I"){
         e.target.parentElement.parentElement.remove()
-        countValue=countValue-1;
+        countValue--;
         count.innerHTML = `todo count : ${countValue}`;
         saveData()
     }
 })
-
-
-
-
-
-
 
 function saveData(){
     localStorage.setItem("data",listBody.innerHTML);
@@ -63,6 +45,7 @@ function saveData(){
 function loadData(){
     listBody.innerHTML=localStorage.getItem("data")
     countValue=localStorage.getItem("count")
+    count.innerHTML = `todo count : ${countValue}`;
 }
 
 loadData();
